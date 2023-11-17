d6ad62eb586cbf96321ffdf4113d1d50857e7acc
Bobkova Maryna <marih@bk.ru>
2023-11-17 11:26:17 +0100

    add y

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Orange orange = new Orange();
         int x = 10;
+        int y = 5;
         System.out.println(x);
+        System.out.println(y);
 
 
     }
